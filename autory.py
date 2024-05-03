74054655e3ad0bfedffd32d85e1a58c083e3b548
calipsso <kamil.vizy@gmail.com>
2024-05-03 15:35:44 +0200

    Test funkcnpsti OK, zatial nie hotovo

--- autory.py
+++
@@ -1,8 +0,0 @@
-from main import db  # Import db prenesený do modulu autory
-
-class Author(db.Model):
-    __tablename__ = "authors"
-
-    author_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    bio = db.Column(db.Text)
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
 
 
 app = Flask(__name__)
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://urici3i0icy8cuufham5:FrtbL6FqnCsROZMWocDBkgoOeoZC7R@b81s1xhecmfxnpmteb27-postgresql.services.clever-cloud.com:50013/b81s1xhecmfxnpmteb27"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-import autory
+
+class Author(db.Model):
+        __tablename__ = "authors"
+
+        author_id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(100), nullable=False)
+        bio = db.Column(db.Text)
 
 @app.route("/authores/", methods=['GET'])
 def getAutorov():
-    autor = autory.Author.query.all()
-    return autor
+    autor = Author.query.all()
+    serialized_authors = [{"author_id": author.author_id, "name": author.name, "bio": author.bio} for author in autor]
+    return jsonify(serialized_authors)
+    #return autor
 
 
 if __name__ == "__main__":
